03f5869c8bf99eb359e14fcd41e29443b6a3b1ce
farrel30 <98472560+farrel30@users.noreply.github.com>
2022-06-10 21:55:57 +0700

    Create soal nomor 5.py

---
+++ pertemuan-3/soal nomor 5.py
@@ -0,0 +1,6 @@
+#soal nomor 5
+angka = int(input("masukkan tahun : "))
+if angka%4==0:
+  print("tahun ", angka, " adalah tahun kabisat")
+else:
+  print("tahun ", angka, " adalah bukan tahun kabisat")
